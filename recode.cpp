091cf8d3fa108cd0ff14d9e201492efe70e421f3
Daniel Kang <daniel.d.kang@gmail.com>
2016-06-23 04:05:12 -0700

    Reformat class h264_symbol

--- recode.cpp
+++ recode.cpp
@@ -1096,44 +1096,46 @@ class h264_model {
 };
 
 class h264_symbol {
-public:
-  h264_symbol(int symbol, const void*state)
-    : symbol(symbol), state(state) {
+ public:
+  h264_symbol(int symbol, const void *state)
+      : symbol(symbol), state(state) {
   }
 
-  template <class T>
+  template<class T>
   void execute(T &encoder, h264_model *model,
-      Recoded::Block *out, std::vector<uint8_t> &encoder_out) {
+               Recoded::Block *out, std::vector <uint8_t> &encoder_out) {
     bool in_significance_map = (model->coding_type == PIP_SIGNIFICANCE_MAP);
     bool block_of_interest = (model->sub_mb_cat == 1 || model->sub_mb_cat == 2);
     bool print_priors = in_significance_map && block_of_interest;
     if (model->coding_type != PIP_SIGNIFICANCE_EOB) {
-      size_t billable_bytes = encoder.put(symbol, [&](range_t range){
-          return model->probability_for_state(range, state); });
+      size_t billable_bytes = encoder.put(symbol, [&](range_t range) {
+        return model->probability_for_state(range, state);
+      });
       if (billable_bytes) {
         model->billable_bytes(billable_bytes);
       }
-    }else if (block_of_interest) {
-        if (symbol) {
-            LOG_NEIGHBORS("\n");
-        }
+    } else if (block_of_interest) {
+      if (symbol) {
+        LOG_NEIGHBORS("\n");
+      }
     }
     if (print_priors) {
-        model->enable_debug();
+      model->enable_debug();
     }
     model->update_state(symbol, state);
     if (print_priors) {
-        LOG_NEIGHBORS("%d ", symbol);
-        model->disable_debug();
+      LOG_NEIGHBORS("%d ", symbol);
+      model->disable_debug();
     }
     if (state == &model->terminate_context && symbol) {
       encoder.finish();
       out->set_cabac(&encoder_out[0], encoder_out.size());
     }
   }
-private:
+
+ private:
   int symbol;
-  const void* state;
+  const void *state;
 };
 
 class compressor {
